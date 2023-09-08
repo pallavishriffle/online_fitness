3318d03781711acf93e2c6bffc0e3b7dba10cf9a
Pallavi Mourya <pallavim@shriffle.com>
2023-09-08 16:00:39 +0530

    updation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,24 +1,28 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  include JsonWebToken
+    include JsonWebToken
 
-  before_action :authenticate_request
+	before_action :authenticate_request
 
   def authenticate_request
-    header = request.headers['Authorization']
-    header = header.split(' ').last if header
-    decoded = jwt_decode(header)
-    @current_user = User.find(decoded[:user_id])
-  rescue JWT::DecodeError
-    render json: { error: 'Invalid token' }, status: :unprocessable_entity
-  rescue ActiveRecord::RecordNotFound
-    render json: 'No record found..'
+    begin
+      header = request.headers['Authorization']
+      header = header.split(" ").last if header
+      decoded = jwt_decode(header)
+      @current_user = User.find(decoded[:user_id])
+    rescue JWT::DecodeError => e
+      render json: { error: 'Invalid token' }, status: :unprocessable_entity
+    rescue ActiveRecord::RecordNotFound
+      render json: "No record found.."
+    end
   end
 
-  attr_reader :current_user
+	def current_user
+		@current_user
+	end
 
   def render_404
-    render json: { error: 'Invalid URL' }, status: :not_found
+    render json: { error: "Invalid URL" }, status: :not_found
   end
 end
