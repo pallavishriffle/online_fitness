f0caaec1c493282ca8381bef08c6dfc26ab1330c
Pallavi Mourya <pallavim@shriffle.com>
2023-09-11 17:00:44 +0530

    updates

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,6 +3,5 @@
 class Category < ApplicationRecord
   belongs_to :user
   has_many :programs, dependent: :destroy
-
   validates :category_name, presence: true, uniqueness: { case_sensitive: false }
 end
